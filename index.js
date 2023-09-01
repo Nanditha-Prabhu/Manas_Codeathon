ace3019a10242e4f3cf0ab086fad0ff5d5d39df2
satwikkamath <107809929+satwikkamath@users.noreply.github.com>
2023-09-01 18:26:29 +0530

    reset and exit button

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ app.use(express.static("public"));
 app.get("/", function (req, res) {
     res.render("main", { level: 1, userName: "Satwik Kamath", score: 0, numbers: [3, 6, 5], formPost: "second", options: [{ option: 355, value: 0 }, { option: 368, value: 0 }, { option: 362, value: 0 }, { option: 365, value: 1 }] });
 })
-
 app.post("/second", function (req, res) {
     userName = req.body.userName;
     ans = req.body.ans;
@@ -38,7 +37,7 @@ app.post("/fourth", function (req, res) {
 
     if (ans == "1")
         score++;
-    res.render("main", { level: 4, userName: userName, score: score, numbers: [2, 4, 7, 3, 9, 2], formPost: "fifth", options: [{ option: 247492, value: 1 }, { option: 247392, value: 0 }, { option: 247385, value: 0 }, { option: 247393, value: 0 }] });
+    res.render("main", { level: 4, userName: userName, score: score, numbers: [2, 4, 7, 3, 9, 2], formPost: "fifth", options: [{ option: 247492, value: 0 }, { option: 247392, value: 1 }, { option: 247385, value: 0 }, { option: 247393, value: 0 }] });
 })
 app.post("/fifth", function (req, res) {
     userName = req.body.userName;
@@ -68,8 +67,21 @@ app.post("/seventh", function (req, res) {
     res.render("main", { level: 7, userName: userName, score: score, numbers: [5, 2, 3, 9, 1, 8, 7, 4, 6], formPost: "end", options: [{ option: 523918746, value: 1 }, { option: 523918745, value: 0 }, { option: 523918749, value: 0 }, { option: 523918744, value: 0 }] });
 
 })
-app.post("/end",function(req,res){
+app.post("/end", function (req, res) {
     res.render("end");
+})
+
+app.post("/reset",function(req,res){
+    userName = req.body.userName;
+
+    res.render("main", { level: 1, userName: userName, score: 0, numbers: [3, 6, 5], formPost: "second", options: [{ option: 355, value: 0 }, { option: 368, value: 0 }, { option: 362, value: 0 }, { option: 365, value: 1 }] });
+
+})
+app.post("/exit",function(req,res){
+    userName = req.body.userName;
+
+    res.render("end");
+
 })
 app.listen(3000, function () {
     console.log("Running");
