bba8b16ebd01ba0b02e8789fefcc6ed699742b68
satwikkamath <107809929+satwikkamath@users.noreply.github.com>
2023-09-05 15:43:29 +0530

    updated playgroound

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ app.post("/checkSignUpInfo",function(req,res){
 })
 
 app.get("/level1",function(req,res){
-    res.render("playground")
+    res.render("playground",{ level: 2, userName: "Satwik", score: 0, numbers: [4, 6, 2, 9], formPost: "third", options: [{ option: 4620, value: 0 }, { option: 4629, value: 1 }, { option: 4649, value: 0 }, { option: 4639, value: 0 }] })
 })
 app.post("/second", function (req, res) {
     userName = req.body.userName;
@@ -76,7 +76,7 @@ app.post("/second", function (req, res) {
     if (ans == "1")
         score++;
 
-    res.render("main", { level: 2, userName: userName, score: score, numbers: [4, 6, 2, 9], formPost: "third", options: [{ option: 4620, value: 0 }, { option: 4629, value: 1 }, { option: 4649, value: 0 }, { option: 4639, value: 0 }] });
+    res.render("playground", { level: 2, userName: userName, score: score, numbers: [4, 6, 2, 9], formPost: "third", options: [{ option: 4620, value: 0 }, { option: 4629, value: 1 }, { option: 4649, value: 0 }, { option: 4639, value: 0 }] });
 })
 
 app.post("/third", function (req, res) {
@@ -86,7 +86,7 @@ app.post("/third", function (req, res) {
 
     if (ans == "1")
         score++;
-    res.render("main", { level: 3, userName: userName, score: score, numbers: [1, 8, 5, 2, 6], formPost: "fourth", options: [{ option: 18526, value: 1 }, { option: 18525, value: 0 }, { option: 18662, value: 0 }, { option: 18523, value: 0 }] });
+    res.render("playground", { level: 3, userName: userName, score: score, numbers: [1, 8, 5, 2, 6], formPost: "fourth", options: [{ option: 18526, value: 1 }, { option: 18525, value: 0 }, { option: 18662, value: 0 }, { option: 18523, value: 0 }] });
 
 })
 app.post("/fourth", function (req, res) {
@@ -96,7 +96,7 @@ app.post("/fourth", function (req, res) {
 
     if (ans == "1")
         score++;
-    res.render("main", { level: 4, userName: userName, score: score, numbers: [2, 4, 7, 3, 9, 2], formPost: "fifth", options: [{ option: 247492, value: 0 }, { option: 247392, value: 1 }, { option: 247385, value: 0 }, { option: 247393, value: 0 }] });
+    res.render("playground", { level: 4, userName: userName, score: score, numbers: [2, 4, 7, 3, 9, 2], formPost: "fifth", options: [{ option: 247492, value: 0 }, { option: 247392, value: 1 }, { option: 247385, value: 0 }, { option: 247393, value: 0 }] });
 })
 app.post("/fifth", function (req, res) {
     userName = req.body.userName;
@@ -105,7 +105,7 @@ app.post("/fifth", function (req, res) {
 
     if (ans == "1")
         score++;
-    res.render("main", { level: 5, userName: userName, score: score, numbers: [2, 7, 5, 3, 9, 6, 7], formPost: "sixth", options: [{ option: 2753969, value: 0 }, { option: 2753968, value: 0 }, { option: 2753967, value: 1 }, { option: 2753963, value: 0 }] });
+    res.render("playground", { level: 5, userName: userName, score: score, numbers: [2, 7, 5, 3, 9, 6, 7], formPost: "sixth", options: [{ option: 2753969, value: 0 }, { option: 2753968, value: 0 }, { option: 2753967, value: 1 }, { option: 2753963, value: 0 }] });
 })
 app.post("/sixth", function (req, res) {
     userName = req.body.userName;
@@ -114,7 +114,7 @@ app.post("/sixth", function (req, res) {
 
     if (ans == "1")
         score++;
-    res.render("main", { level: 6, userName: userName, score: score, numbers: [2, 9, 5, 8, 6, 4, 7, 3], formPost: "seventh", options: [{ option: 29586474, value: 0 }, { option: 29586473, value: 1 }, { option: 29586479, value: 0 }, { option: 29586476, value: 0 }] });
+    res.render("playground", { level: 6, userName: userName, score: score, numbers: [2, 9, 5, 8, 6, 4, 7, 3], formPost: "seventh", options: [{ option: 29586474, value: 0 }, { option: 29586473, value: 1 }, { option: 29586479, value: 0 }, { option: 29586476, value: 0 }] });
 })
 app.post("/seventh", function (req, res) {
     userName = req.body.userName;
@@ -123,7 +123,7 @@ app.post("/seventh", function (req, res) {
 
     if (ans == "1")
         score++;
-    res.render("main", { level: 7, userName: userName, score: score, numbers: [5, 2, 3, 9, 1, 8, 7, 4, 6], formPost: "end", options: [{ option: 523918746, value: 1 }, { option: 523918745, value: 0 }, { option: 523918749, value: 0 }, { option: 523918744, value: 0 }] });
+    res.render("playground", { level: 7, userName: userName, score: score, numbers: [5, 2, 3, 9, 1, 8, 7, 4, 6], formPost: "end", options: [{ option: 523918746, value: 1 }, { option: 523918745, value: 0 }, { option: 523918749, value: 0 }, { option: 523918744, value: 0 }] });
 
 })
 app.post("/end", function (req, res) {
